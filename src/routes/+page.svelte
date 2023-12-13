6e23b969b11481c1710c937d3933821cf5a2f6d9
Josh Bacon <bacon.josh09@gmail.com>
2023-12-13 05:23:36 -0500

    Fix imports that got messed up when moving file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import PreMatch from '$lib/gameStates/PreMatch.svelte';
-	import Autonomous from '../../lib/gameStates/Autonomous.svelte';
+	import Autonomous from '$lib/gameStates/Autonomous.svelte';
 	import { matchPhase } from '$lib/stores';
 	import { MatchPhase } from '$lib/types';
-	import Teleop from '../../lib/gameStates/Teleop.svelte';
-	import PostMatch from '../../lib/gameStates/PostMatch.svelte';
+	import Teleop from '$lib/gameStates/Teleop.svelte';
+	import PostMatch from '$lib/gameStates/PostMatch.svelte';
 </script>
 
 <div class="px-4 h-full w-full max-w-xl">
