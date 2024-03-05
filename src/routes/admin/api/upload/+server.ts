96af5f1e8ed84fda598bc7135c77b2ef245f7903
Josh Bacon <bacon.josh09@gmail.com>
2024-03-04 20:03:06 -0500

    Add Lydia as uploader

--- src/routes/admin/api/upload/+server.ts
+++ src/routes/admin/api/upload/+server.ts
@@ -6,7 +6,8 @@ const usersWhoCanUpload: string[] = [
 	'fd198f8f-b4fd-4595-a0cb-cd4bc2c1088a', // Mark
 	'7e656f77-e4c0-44f1-92b3-210c93195675', // Blake
 	'437f5379-650b-4f25-9ed4-1f0e4b1d83b6', // Rylee
-	'43f02320-89ed-4f3f-abf4-340566ae8697' // AJ
+	'43f02320-89ed-4f3f-abf4-340566ae8697', // AJ
+	'9af0ddf3-2cd7-4d55-b46b-57f501fbeedf' // Lydia
 ];
 
 export async function POST({ request, locals: { supabase, getSession } }) {
